92d97e5c5c760da8c9971f3d289037fcc60e5139
Andrei Murashkin <amuraskin323@gmail.com>
2023-03-15 21:54:17 +0200

    add template

---
+++ src/main/java/com/example/HelsinkiCityBikeApp/HelsinkiCityBikeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelsinkiCityBikeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelsinkiCityBikeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelsinkiCityBikeAppApplication.class, args);
+	}
+
+}
